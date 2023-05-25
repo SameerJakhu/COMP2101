a9f12be56ee077d2c0498397e83bf7b064040c06
sameer <200523749@student.georgianc.on.ca>
2023-05-24 20:49:41 -0400

    Lab 2

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,18 +1,23 @@
-
 #!/bin/bash
 
-# Displaying the fully qualified domain name 
-echo "FQDN:"
-hostname --fqdn
+# Storing the Variables
+hostname=$(hostname)
+fqdn=$(hostname -f)
+os_name=$(lsb_release -ds)
+ip_add=$(ip route get 8.8.8.8 | awk 'NR==1{print $7}')
+disk_free=$(df -h / | awk 'NR==2{print $4}')
 
-# Displaying the os name and version
-echo "Host Information:"
-hostnamectl
+# Output Template
 
-# Displaying the IP address of the host
-echo "IP Addresses:"
-hostname -I
+cat <<EOF
 
-# Displaying the space in the root file system
-echo "Root Filesystem Status:"
-df -h /
+Report for $hostname
+====================
+FQDN: $fqdn
+Operating System name and version: $os_name
+IP Address: $ip_add
+Root Filesystem Free Space: $disk_free
+====================
+
+EOF
+
