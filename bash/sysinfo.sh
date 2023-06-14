24b39cf7e85e282a706cd75c1e46ad142af729cd
sameer <200523749@student.georgianc.on.ca>
2023-06-14 02:12:21 -0400

    Lab 4

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,100 +1,170 @@
 #!/bin/bash
 
-# Check if the user has root privileges
+# Check if the user has root privilege
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run with root privileges."
-    exit 1
+  echo "This script must be run with root privileges."
+  exit 1
 fi
 
+###############
 # System Description
+###############
+
 echo "System Description"
-echo "------------------"
+echo "=================="
 
-manufacturer=$(dmidecode -s system-manufacturer 2>/dev/null)
-if [[ -z $manufacturer ]]; then
-    echo "Computer manufacturer: Data unavailable"
+# Get computer manufacturer
+manufacturer=$(dmidecode -s system-manufacturer)
+if [[ -n $manufacturer ]]; then
+  echo "Computer Manufacturer: $manufacturer"
 else
-    echo "Computer manufacturer: $manufacturer"
+  echo "Error: Unable to retrieve computer manufacturer."
 fi
 
-description=$(dmidecode -s system-product-name 2>/dev/null)
-if [[ -z $description ]]; then
-    echo "Computer description or model: Data unavailable"
+# Get computer description or model
+model=$(dmidecode -s system-product-name)
+if [[ -n $model ]]; then
+  echo "Computer Model: $model"
 else
-    echo "Computer description or model: $description"
+  echo "Error: Unable to retrieve computer model."
 fi
 
-serial_number=$(dmidecode -s system-serial-number 2>/dev/null)
-if [[ -z $serial_number ]]; then
-    echo "Computer serial number: Data unavailable"
+# Get computer serial number
+serial=$(dmidecode -s system-serial-number)
+if [[ -n $serial ]]; then
+  echo "Serial Number: $serial"
 else
-    echo "Computer serial number: $serial_number"
+  echo "Error: Unable to retrieve serial number."
 fi
 
-echo
-
+###############
 # CPU Information
-echo "CPU Information"
-echo "---------------"
-
-cpu_manufacturer=$(lshw -class processor | awk '/product/ {print $2}' 2>/dev/null)
-if [[ -z $cpu_manufacturer ]]; then
-    echo "CPU manufacturer: Data unavailable"
-else
-    echo "CPU manufacturer: $cpu_manufacturer"
-fi
-
-cpu_model=$(lshw -class processor | awk '/product/ {print $3}' 2>/dev/null)
-if [[ -z $cpu_model ]]; then
-    echo "CPU model: Data unavailable"
-else
-    echo "CPU model: $cpu_model"
-fi
-
-cpu_architecture=$(lshw -class processor | awk '/width/ {print $2}' 2>/dev/null)
-if [[ -z $cpu_architecture ]]; then
-    echo "CPU architecture: Data unavailable"
-else
-    echo "CPU architecture: $cpu_architecture"
-fi
-
-cpu_core_count=$(lshw -class processor | awk '/core/ {print $4}' | head -n 1 2>/dev/null)
-if [[ -z $cpu_core_count ]]; then
-    echo "CPU core count: Data unavailable"
-else
-    echo "CPU core count: $cpu_core_count"
-fi
-
-cpu_max_speed=$(lshw -class processor | awk '/capacity/ {print $2}' | head -n 1 2>/dev/null)
-if [[ -z $cpu_max_speed ]]; then
-    echo "CPU maximum speed: Data unavailable"
-else
-    echo "CPU maximum speed: $cpu_max_speed"
-fi
-
-cache_sizes=$(lshw -class processor | awk '/size/ {print $2}' | head -n 3 2>/dev/null)
-if [[ -z $cache_sizes ]]; then
-    echo "Cache sizes (L1, L2, L3): Data unavailable"
-else
-    echo "Cache sizes (L1, L2, L3):"
-    echo "$cache_sizes"
-fi
+###############
 
 echo
+echo "CPU Information"
+echo "==============="
 
-# Operating System Information
-echo "Operating System Information"
-echo "---------------------------"
-
-linux_distro=$(lsb_release -ds 2>/dev/null)
-if [[ -z $linux_distro ]]; then
-    echo "Linux distro: Data unavailable"
+# Get CPU information
+cpu_info=$(lshw -class processor 2>/dev/null)
+if [[ -n $cpu_info ]]; then
+  cpu_count=$(grep -c "product" <<< "$cpu_info")
+  if [[ $cpu_count -gt 0 ]]; then
+    i=0
+    while IFS= read -r line; do
+      if [[ $line == *"product:"* ]]; then
+        i=$((i+1))
+        cpu_model=$(awk -F ': ' '{print $2}' <<< "$line")
+        echo "CPU $i: $cpu_model"
+      elif [[ $line == *"configuration: cores="* ]]; then
+        cpu_cores=$(awk -F 'cores=' '{print $2}' <<< "$line")
+        echo "    Cores: $cpu_cores"
+      elif [[ $line == *"clock"* ]]; then
+        cpu_speed=$(awk -F 'clock: ' '{print $2}' <<< "$line")
+        echo "    Maximum Speed: $cpu_speed"
+      elif [[ $line == *"size"* ]]; then
+        cache_size=$(awk -F 'size: ' '{print $2}' <<< "$line")
+        cache_unit=$(awk -F 'size: ' '{print $2}' <<< "$line")
+        echo "    Cache Size: $cache_size $cache_unit"
+      fi
+    done <<< "$cpu_info"
+  else
+    echo "Error: Unable to retrieve CPU information."
+  fi
 else
-    echo "Linux distro: $linux_distro"
+  echo "Error: Unable to retrieve CPU information."
 fi
 
-distro_version=$(lsb_release -rs 2>/dev/null)
-if [[ -z $distro_version ]]; then
-    echo "Distro version: Data unavailable"
+###############
+# Operating System Information
+###############
+
+echo
+echo "Operating System Information"
+echo "============================"
+
+# Get Linux distro
+linux_distro=$(lsb_release -ds)
+if [[ -n $linux_distro ]]; then
+  echo "Linux Distro: $linux_distro"
 else
-    echo "Distro version: $dist
+  echo "Error: Unable to retrieve Linux distro information."
+fi
+
+# Get distro version
+distro_version=$(lsb_release -rs)
+if [[ -n $distro_version ]]; then
+  echo "Distro Version: $distro_version"
+else
+  echo "Error: Unable to retrieve distro version information."
+fi
+
+###############
+# RAM Information
+###############
+
+echo
+echo "RAM Information"
+echo "==============="
+
+# Get installed memory components
+memory_info=$(lshw -class memory 2>/dev/null)
+if [[ -n $memory_info ]]; then
+  echo "Component Manufacturer  | Component Model   | Component Size  | Component Speed  | Component Location"
+  echo "-----------------------|-------------------|-----------------|------------------|------------------"
+  while IFS= read -r line; do
+    if [[ $line == *"description: "* ]]; then
+      manufacturer=$(awk -F 'description: ' '{print $2}' <<< "$line")
+    elif [[ $line == *"product: "* ]]; then
+      model=$(awk -F 'product: ' '{print $2}' <<< "$line")
+    elif [[ $line == *"size: "* ]]; then
+      size=$(awk -F 'size: ' '{print $2}' <<< "$line")
+      size_unit=$(awk -F 'size: ' '{print $2}' <<< "$line")
+    elif [[ $line == *"clock: "* ]]; then
+      speed=$(awk -F 'clock: ' '{print $2}' <<< "$line")
+    elif [[ $line == *"location: "* ]]; then
+      location=$(awk -F 'location: ' '{print $2}' <<< "$line")
+      echo "$manufacturer           | $model           | $size $size_unit | $speed           | $location"
+    fi
+  done <<< "$memory_info"
+else
+  echo "Error: Unable to retrieve RAM information."
+fi
+
+# Get total size of installed RAM
+total_ram=$(grep -m 1 "size: " <<< "$memory_info" | awk -F 'size: ' '{print $2}')
+total_ram_unit=$(grep -m 1 "size: " <<< "$memory_info" | awk -F 'size: ' '{print $2}')
+if [[ -n $total_ram ]]; then
+  echo
+  echo "Total Installed RAM: $total_ram $total_ram_unit"
+else
+  echo "Error: Unable to retrieve total installed RAM."
+fi
+
+###############
+# Disk Storage Information
+###############
+
+echo
+echo "Disk Storage Information"
+echo "========================"
+
+# Get installed disk drives
+disk_info=$(lshw -class disk 2>/dev/null)
+if [[ -n $disk_info ]]; then
+  echo "Drive Manufacturer   | Drive Model        | Drive Size  | Partition | Mount Point | Filesystem Size | Filesystem Free Space"
+  echo "---------------------|--------------------|-------------|-----------|-------------|-----------------|---------------------"
+  while IFS= read -r line; do
+    if [[ $line == *"vendor: "* ]]; then
+      manufacturer=$(awk -F 'vendor: ' '{print $2}' <<< "$line")
+    elif [[ $line == *"product: "* ]]; then
+      model=$(awk -F 'product: ' '{print $2}' <<< "$line")
+    elif [[ $line == *"size: "* ]]; then
+      size=$(awk -F 'size: ' '{print $2}' <<< "$line")
+      size_unit=$(awk -F 'size: ' '{print $2}' <<< "$line")
+    elif [[ $line == *"logical name: /dev/"* ]]; then
+      partition=$(awk -F 'logical name: /dev/' '{print $2}' <<< "$line")
+      mount_point=$(df -h | awk -v p="/dev/$partition" '$1==p{print $6}')
+      fs_size=$(df -h | awk -v p="/dev/$partition" '$1==p{print $2}')
+      fs_free=$(df -h | awk -v p="/dev/$partition" '$1==p{print $4}')
+      echo "$manufacturer      | $model        | $size $size_unit | $partition  | $mount_point | $fs_size         | $
