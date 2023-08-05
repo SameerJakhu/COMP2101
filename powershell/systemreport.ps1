00727de5bc082763fd38c9cddf79d0b5305c459f
SameerJakhu <133973841+SameerJakhu@users.noreply.github.com>
2023-08-05 15:49:08 -0400

    Create systemreport.ps1

---
+++ powershell/systemreport.ps1
@@ -0,0 +1,30 @@
+﻿param (
+    [switch]$System,
+    [switch]$Disks,
+    [switch]$Network
+)
+
+if (-not ($System -or $Disks -or $Network)) {
+    # If no parameters provided, generate the full report
+    Get-CpuInfo
+    Get-OSInfo
+    Get-RamInfo
+    Get-VideoInfo
+    Get-DisksInfo
+    Get-NetworkInfo
+}
+else {
+    # Generate reports based on provided parameters
+    if ($System) {
+        Get-CpuInfo
+        Get-OSInfo
+        Get-RamInfo
+        Get-VideoInfo
+    }
+    if ($Disks) {
+        Get-DisksInfo
+    }
+    if ($Network) {
+        Get-NetworkInfo
+    }
+}
